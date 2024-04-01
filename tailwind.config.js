e5e725a7c479856b0a20f3a875878f57b54fa2a8
Icaro <icaro.onofre.s@gmail.com>
2024-04-01 20:01:47 -0300

    Esboçando layout,add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
+    colors:{
+		background:'#FFF9F9',
+		mainRed:'#DF2828',
+		lightRed:'#FFE8E4',
+		white:'white',
+		blue:'#5863F8',
+    	yellow:'#ECA72C',
+    	background:'#F3F4FE',
+    },
+
   },
   plugins: [],
 }
